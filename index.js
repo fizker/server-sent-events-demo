e6044bb92503f410174347973fbf22f08110c4dc
Benjamin Horsleben <benjamin@fizkerinc.dk>
2021-03-14 14:50:44 +0100

    Added basic client-side page

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 import express from "express"
+import path from "path"
 
+const __dirname = path.dirname(import.meta.url).replace(/^file:\/\//, "")
 const app = express()
 const port = process.env.PORT ?? 8030
 
 app.get("/", (req, res) => {
-	res.send("hello world")
+	res.sendFile(path.join(__dirname, "client/index.html"))
 })
 
-app.post("/messages", express.json(), (req, res) => {
+app.post("/messages", express.json(), express.urlencoded({ extended: true }), (req, res) => {
 	const message = req.body
 
 	if(message.name == null || typeof message.name !== "string") {
 		// Error out
 		res.status(400).send("Message must have `name` attribute")
 		return
+	}
+
+	if(message.data === "") {
+		message.data = null
 	}
 
 	console.log({ message })
