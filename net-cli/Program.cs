f309dbae1bbd0ff5379cd98c143491170a5feb73
Benjamin Horsleben <benjamin@fizkerinc.dk>
2021-03-14 18:18:33 +0100

    Added CLI .NET client

--- net-cli/Program.cs
+++ net-cli/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using LaunchDarkly.EventSource;
 
 namespace net_cli
 {
@@ -6,7 +7,47 @@ namespace net_cli
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Hit enter to exit");
+			var controller = new EventSourceController(new("http://localhost:8030/messages"));
+			controller.Connect();
+			Console.ReadLine();
+		}
+	}
+
+	class EventSourceController
+	{
+		readonly EventSource _eventSource;
+
+		public EventSourceController(Uri serverUri)
+		{
+			var configuration = Configuration.Builder(serverUri)
+				.Build();
+			_eventSource = new(configuration);
+
+			_eventSource.Opened += OnOpen;
+			_eventSource.MessageReceived += OnMessage;
+			_eventSource.Error += OnError;
+		}
+
+		public void Connect()
+		{
+			_ = _eventSource.StartAsync();
+		}
+
+		void OnOpen(object sender, StateChangedEventArgs e)
+		{
+			Console.WriteLine("Connection open");
+		}
+
+		void OnMessage(object sender, MessageReceivedEventArgs e)
+		{
+			Console.WriteLine($"Event name: {e.EventName}");
+			Console.WriteLine($"Data: {e.Message.Data}");
+		}
+
+		void OnError(object sender, ExceptionEventArgs e)
+		{
+			Console.WriteLine($"Got exception: ({e.Exception.GetType().FullName}) {e.Exception.Message}");
 		}
 	}
 }
