b0ea551e9251aa7c42cecd91fab3293bfbfb3d1a
Benjamin Horsleben <benjamin@fizkerinc.dk>
2022-02-03 09:25:17 +0100

    Updated the main to support standard PORT env var

--- swift-server/Sources/EventSourceServer/main.swift
+++ swift-server/Sources/EventSourceServer/main.swift
@@ -1,12 +1,15 @@
 import Vapor
 
+let pi = ProcessInfo.processInfo
+let port = pi.environment["PORT"] ?? "8031"
+
 var env = try Environment.detect()
 if env.arguments.count == 1 {
 	env.arguments.append("serve")
 }
 if env.arguments[1] == "serve" && !env.arguments.contains("--port") {
 	env.arguments.append("--port")
-	env.arguments.append("8031")
+	env.arguments.append(port)
 }
 try LoggingSystem.bootstrap(from: &env)
 let app = Application(env)
