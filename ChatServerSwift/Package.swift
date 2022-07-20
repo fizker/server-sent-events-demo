135721fa8998c1cd843120e921e4954bd85d0cbb
Benjamin Horsleben <benjamin@fizkerinc.dk>
2022-07-20 14:21:11 +0200

    Wired up a SQLite in-memory database for the ChatDemo Swift server

--- ChatServerSwift/Package.swift
+++ ChatServerSwift/Package.swift
@@ -18,6 +18,8 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/vapor/vapor.git", from: "4.62.1"),
+		.package(url: "https://github.com/vapor/fluent.git", from: "4.4.0"),
+		.package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.2.0"),
 		.package(url: "https://github.com/fizker/swift-server-sent-event-models.git", from: "0.0.1"),
 	],
 	targets: [
@@ -25,6 +27,8 @@ let package = Package(
 			name: "ServerSentEventVapor",
 			dependencies: [
 				.product(name: "Vapor", package: "vapor"),
+				.product(name: "Fluent", package: "fluent"),
+				.product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
 				.product(name: "ServerSentEventModels", package: "swift-server-sent-event-models"),
 			]
 		),
